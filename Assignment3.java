9601cf2976d991f69ea7131ab011d31b34085fae
Samuel A. Uganiza <sau23@users.noreply.github.com>
2021-02-25 11:03:45 -0500

    fix last answer

--- Assignment3.java
+++ Assignment3.java
@@ -10,7 +10,7 @@ public class Assignment3 {
 		testIfElseIf();
 		testOutOfRange();
 		printGrade();
-		printGradeSwitch();
+		printWeekdaySwitch();
 	}
 
 	/**
@@ -150,35 +150,35 @@ public class Assignment3 {
 	 * greater than 7. Don’t forget to include “break” statements in 
 	 * each of your cases.
 	 */
-	private static void printGradeSwitch() {
-		System.out.println("printGradeSwitch:");
+	private static void printWeekdaySwitch() {
+		System.out.println("printWeekdaySwitch:");
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Please enter a number between 0 and 100: ");
+		System.out.print("Please enter a number between 1 and 7: ");
 		int input = sc.nextInt();
 		switch(input) {
-			case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: 
-			case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: case 20: 
-			case 21: case 22: case 23: case 24: case 25: case 26: case 27: case 28: case 29: case 30: 
-			case 31: case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 40: 
-			case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: case 50: 
-			case 51: case 52: case 53: case 54: case 55: case 56: case 57: case 58: case 59: 
-				System.out.println("F");
+			case 1:
+				System.out.println("Monday");
 				break;
-			case 60: case 61: case 62: case 63: case 64: case 65: case 66: case 67: case 68: case 69:
-				System.out.println("D");
+			case 2:
+				System.out.println("Tuesday");
 				break;
-			case 70: case 71: case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 79:
-				System.out.println("C");
+			case 3:
+				System.out.println("Wednesday");
 				break;
-			case 80: case 81: case 82: case 83: case 84: case 85: case 86: case 87: case 88: case 89:
-				System.out.println("B");
+			case 4:
+				System.out.println("Thursday");
 				break;
-			case 90: case 91: case 92: case 93: case 94: case 95: case 96: case 97: case 98: case 99:
-			case 100:
-				System.out.println("A");
+			case 5:
+				System.out.println("Friday");
+				break;
+			case 6:
+				System.out.println("Saturday");
+				break;
+			case 7:
+				System.out.println("Sunday");
 				break;
 			default:
-				System.out.println("Score out of range.");	
+				System.out.println("Out of range");
 		}
 		sc.close();
 	}
